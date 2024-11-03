b6a700775e263094c7785fc1c87b70cf173a415f
DevMGcode <meli.bogar15@gmail.com>
2024-11-02 23:24:16 -0500

    screenmatchphrases

---
+++ screenmatch-frases/src/main/java/com/cursos/screenmatch_frases/FraseController.java
@@ -0,0 +1,18 @@
+package com.cursos.screenmatch_frases;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FraseController {
+
+    @Autowired
+    FraseService servicio;
+
+    @GetMapping("/series/frases")
+    public FraseDTO obtenerFraseAleatoria(){
+      return servicio.obtenerFraseAleatoria();
+    }
+}
